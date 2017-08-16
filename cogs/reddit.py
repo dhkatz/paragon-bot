6b9f7ad2b5d4dcf3144a817ff8100f665b444abd
DoctorJew <thedoctorjew@gmail.com>
2017-08-16 16:29:22 -0700

    Fixed some stuff. Started working on reddit cog.

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -1,10 +1,27 @@
 from discord.ext import commands
+import praw
+import logging
+import config.load as config
 
 
 class Reddit:
-    """Reddit related commands. Designed for use by /r/Paragon moderators only!
-        Requires an authentication key to be used!
-    """
+    """Reddit related commands. Designed for use by /r/Paragon moderators only!"""
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.logger = logging.getLogger('discord')
+        self.user_agent = '/r/Paragon Post Collection Bot'
+        self.reddit = praw.Reddit(user_agent=self.user_agent, client_id=config.__reddit__['client_id'],
+                                  client_secret=config.__reddit__['client_secret'],
+                                  username=config.__reddit__['username'],
+                                  password=config.__reddit__['password'])
+        # Make sure we are logged in!
+        self.logger.info('Logged into Reddit as ' + self.reddit.user.me())
+
+    @commands.command(pass_context=True)
+    async def reddit(self, ctx):
+        print(ctx.message.author)
+
+
+def setup(bot):
+    bot.add_cog(Reddit(bot))
--- config/load.py
+++ config/load.py
@@ -1,6 +1,6 @@
 # LOAD BOT CONFIGURATION
 try:
-    from config.config import __token__, __prefix__, __ownerid__, __cleverbot__
+    from config.config import __token__, __prefix__, __ownerid__, __cleverbot__, __reddit__
 except ImportError:
     import os
 
@@ -8,5 +8,6 @@ except ImportError:
     __prefix__ = os.environ.get(' DISCORD_PREFIX ')
     __ownerid__ = os.environ.get(' DISCORD_OWNERID ')
     __cleverbot__ = os.environ.get(' CLEVERBOT_API ')
+    __reddit__ = os.environ.get(' REDDIT_LOGIN ')
 # LOAD BOT MODULES
 from config.config import __cogs__
