d39bf95ea2282a7abfb7c407c5a94aa4e209d0c0
DoctorJew <thedoctorjew@gmail.com>
2017-09-14 16:55:54 -0700

    Working on some thicc Reddit stuff.

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -2,6 +2,8 @@ import praw, prawcore, discord, asyncio
 from discord.ext import commands
 
 import data.config.load as config
+from .util.paginator import EmbedPages
+import time
 
 
 class Reddit:
@@ -9,20 +11,11 @@ class Reddit:
 
     def __init__(self, bot):
         self.bot = bot
-        self.user_agent = '/r/Paragon Post Collection Bot'
-        self.subreddit_name = 'paragon'
-        self.reddit = None
-        self.subreddit = None
-
-    async def login(self):
-        # self.reddit = praw.Reddit(user_agent=self.user_agent, client_id=config.__reddit__['client_id'],
-        #                           client_secret=config.__reddit__['client_secret'],
-        #                           username=config.__reddit__['username'],
-        #                           password=config.__reddit__['password'])
-        # self.reddit.subreddit(self.subreddit_name)
-        # # Make sure we are logged in!
-        # self.bot.logger.info('Logged into Reddit as ' + str(self.reddit.user.me()))
-        self.bot.logger.info('Test event.')
+        self.instance = praw.Reddit(user_agent='/r/Paragon Discord Bot', client_id=config.__reddit__['client_id'],
+                                    client_secret=config.__reddit__['client_secret'],
+                                    username=config.__reddit__['username'], password=config.__reddit__['password'])
+        self.subreddit = self.instance.subreddit('paragon')
+        self.bot.logger.info('Logged into Reddit as ' + str(self.instance.user.me()))
 
     @commands.group(name='reddit')
     async def reddit(self, ctx):
@@ -30,23 +23,49 @@ class Reddit:
         if ctx.invoked_subcommand is None:
             await ctx.send('Reddit functionality is still a WIP!')
 
-    @reddit.command(name='stickied')
+    @reddit.command(name='announcements')
     async def reddit_stickied(self, ctx):
         """Get the current stickied posts from /r/Paragon!"""
+        embeds = []
         for i in range(1, 3):
             try:
                 sticky = self.subreddit.sticky(number=i)
-
             except prawcore.NotFound:
                 continue
-            embed = discord.Embed()
-            embed.title = sticky.title
-            embed.url = sticky.shortlink
-            await ctx.send(embed=embed)
-            print(vars(sticky))
+            else:
+                embeds.append(self.build_submission_embed(sticky))
+        if len(embeds) == 0:
+            self.bot.embed_notify(ctx, 2, 'Notice', 'There are currently no announcements!')
+            return
+        p = EmbedPages(ctx, icon_url=self.bot.icon_url, entries=embeds)
+        await p.paginate()
+
+    @reddit.command(name='official')
+    async def reddit_official(self, ctx):
+        """Get official posts from Epic Games currently on the front page."""
+        embeds = []
+        for submission in self.subreddit.hot():
+            if str(submission.link_flair_text).upper() == 'OFFICIAL':
+                embeds.append(submission)
+
+        if len(embeds) == 0:
+            self.bot.embed_notify(ctx, 2, 'Notice', 'There are currently no official posts!')
+            return
+
+        p = EmbedPages(ctx, icon_url=self.bot.icon_url, entries=embeds)
+        await p.paginate()
+
+    @staticmethod
+    async def build_submission_embed(submission):
+        embed = discord.Embed()
+        embed.title = submission.title
+        embed.url = submission.shortlink
+        embed.description = submission.selftext[:240] + '...'
+        embed.add_field(name='Author', value=submission.author.name, inline=False)
+        embed.add_field(name='Time', value=time.strftime("%a, %d %b %Y %H:%M", time.gmtime(submission.created_utc)),
+                        inline=False)
+        return embed
 
 
 def setup(bot):
-    n = Reddit(bot)
-    bot.add_listener(n.login, 'on_ready')
-    bot.add_cog(n)
+    bot.add_cog(Reddit(bot))
